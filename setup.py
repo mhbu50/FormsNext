c1a5f8a16fd93b76f9ccbb265908774d6b6a3421
Mohammed Redah <mhbu50@gmail.com>
2026-01-03 13:17:02 +0300

    Refactor setup.py to use __version__ import

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-import re, ast
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in formsnext/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('formsnext/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
-
+from formsnext import __version__ as
 setup(
 	name='formsnext',
 	version=version,
